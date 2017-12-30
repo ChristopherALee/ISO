191d2b4b9379f2f7525efbb71a9a6262dba62be4
ChristopherALee <christopheradamlee@gmail.com>
2017-12-30 18:58:25 -0500

    Created Session API Util file

--- frontend/iso.jsx
+++ frontend/iso.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-window.signUp = (user) => {
-  return $.ajax({
-    method: 'POST',
-    url: 'api/users',
-    data: {
-      user
-    }
-  }).then(
-    user => console.log(user)
-  );
-};
+// TESTING PURPOSES
+import * as SessionApiUtil from './util/session/session_api_util';
 
-window.login = (user) => {
-  return $.ajax({
-    method: 'POST',
-    url: 'api/session',
-    data: {
-      user
-    }
-  }).then(
-    user => console.log(`${user.username} has logged in!`)
-  );
-};
+window.signUp = SessionApiUtil.signUp;
+window.login = SessionApiUtil.login;
+window.logout = SessionApiUtil.logout;
 
-window.logout = () => {
-  return $.ajax({
-    method: 'DELETE',
-    url: 'api/session'
-  }).then(
-    () => console.log('logged out!')
-  );
-};
+// window.signUp = (user) => {
+//   return $.ajax({
+//     method: 'POST',
+//     url: 'api/users',
+//     data: {
+//       user
+//     }
+//   }).then(
+//     user => console.log(user)
+//   );
+// };
+//
+// window.login = (user) => {
+//   return $.ajax({
+//     method: 'POST',
+//     url: 'api/session',
+//     data: {
+//       user
+//     }
+//   }).then(
+//     user => console.log(`${user.username} has logged in!`)
+//   );
+// };
+//
+// window.logout = () => {
+//   return $.ajax({
+//     method: 'DELETE',
+//     url: 'api/session'
+//   }).then(
+//     () => console.log('logged out!')
+//   );
+// };
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
