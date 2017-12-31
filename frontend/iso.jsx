6910b323c672652481669699225655a9588323af
ChristopherALee <christopheradamlee@gmail.com>
2017-12-30 19:26:07 -0500

    Created Store. Fixed an import line of code in errors_reducer and added Store to entry file

--- frontend/iso.jsx
+++ frontend/iso.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 // TESTING PURPOSES START
 import * as SessionApiUtil from './util/session/session_api_util';
@@ -10,6 +11,12 @@ window.logout = SessionApiUtil.logout;
 // TESTING ENDS
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  // TESTING
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // TESTING
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>ISO</h1>, root);
 });
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import sessionErrorsReducer from './session/sessionErrorsReducer';
+import sessionErrorsReducer from './session/session_errors_reducer';
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
