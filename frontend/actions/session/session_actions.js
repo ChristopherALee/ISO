ec484975a5231c3e07e9fdf7ce9727aaf4aa3e34
ChristopherALee <christopheradamlee@gmail.com>
2017-12-30 19:05:44 -0500

    Created Session Actions file

---
+++ frontend/actions/session/session_actions.js
@@ -0,0 +1,33 @@
+import * as SessionApiUtil from '../../util/session/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+export const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_ERRORS,
+    errors
+  };
+};
+
+export const login = user => dispatch => {
+  return (
+    SessionApiUtil.login(user).then(
+      currentUser => dispatch(receiveCurrentUser(currentUser)))
+  );
+};
+
+export const logout = () => dispatch => {
+  return (
+    SessionApiUtil.logout().then(
+      () => dispatch(receiveCurrentUser(null))
+    )
+  );
+};
--- frontend/iso.jsx
+++ frontend/iso.jsx
@@ -1,45 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// TESTING PURPOSES
+// TESTING PURPOSES START
 import * as SessionApiUtil from './util/session/session_api_util';
 
 window.signUp = SessionApiUtil.signUp;
 window.login = SessionApiUtil.login;
 window.logout = SessionApiUtil.logout;
-
-// window.signUp = (user) => {
-//   return $.ajax({
-//     method: 'POST',
-//     url: 'api/users',
-//     data: {
-//       user
-//     }
-//   }).then(
-//     user => console.log(user)
-//   );
-// };
-//
-// window.login = (user) => {
-//   return $.ajax({
-//     method: 'POST',
-//     url: 'api/session',
-//     data: {
-//       user
-//     }
-//   }).then(
-//     user => console.log(`${user.username} has logged in!`)
-//   );
-// };
-//
-// window.logout = () => {
-//   return $.ajax({
-//     method: 'DELETE',
-//     url: 'api/session'
-//   }).then(
-//     () => console.log('logged out!')
-//   );
-// };
+// TESTING ENDS
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
