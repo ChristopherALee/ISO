342c37fbac2afedec03837f13e83e9ea45409022
ChristopherALee <christopheradamlee@gmail.com>
2017-12-31 12:32:05 -0500

    Created App and Root components and implemented them into entry file

---
+++ frontend/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div id='iso-app'>
+      <h1>Welcome to ISO</h1>
+    </div>
+  );
+};
+
+export default App;
--- frontend/iso.jsx
+++ frontend/iso.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 import configureStore from './store/store';
 
 // TESTING PURPOSES START
@@ -18,5 +19,5 @@ document.addEventListener('DOMContentLoaded', () => {
   // TESTING
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>ISO</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
