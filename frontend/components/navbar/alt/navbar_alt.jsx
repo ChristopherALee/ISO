b6861722c3225de813c3614ea5cb7d87b66e52ac
ChristopherALee <christopheradamlee@gmail.com>
2018-01-01 11:57:45 -0500

    Setup Navbar Alt container and component and added a gallery icon for it

---
+++ frontend/components/navbar/alt/navbar_alt.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class NavBarAlt extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <nav id='navbar-alt'>
+        <p>LOGO PLACEHOLDER</p>
+
+      </nav>
+    );
+  }
+}
+
+export default NavBarAlt;
---
+++ frontend/components/navbar/alt/navbar_alt_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import NavBarAlt from './navbar_alt';
+
+const mapStateToProps = (state) => {
+  return {
+
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(NavBarAlt);
