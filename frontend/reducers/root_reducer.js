b430d61c4dc5054646b4b91d252d2f5503e53dbd
ChristopherALee <christopheradamlee@gmail.com>
2018-01-03 14:02:10 -0500

    Added photosReducer to rootReducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import sessionReducer from './session/session_reducer';
 import errorsReducer from './errors_reducer';
+import photosReducer from './photos/photos_reducer';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
-  errors: errorsReducer
+  errors: errorsReducer,
+  photos: photosReducer,
 });
 
 export default rootReducer;
