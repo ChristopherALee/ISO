ea4fb8928a2f229fab310c7ef4ce055e99d91cb8
ChristopherALee <christopheradamlee@gmail.com>
2018-01-03 09:42:06 -0500

    Created Photos model and migration (not migrated yet) and installed imagemagick

---
+++ db/migrate/20180103141953_create_photos.rb
@@ -0,0 +1,13 @@
+class CreatePhotos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :photos do |t|
+      t.string :title, null: false
+      t.text :description
+      t.integer :author_id, null: false
+      t.string :comment_ids, array: true, default: []
+      t.timestamps
+    end
+
+    add_index :photos, :author_id
+  end
+end
