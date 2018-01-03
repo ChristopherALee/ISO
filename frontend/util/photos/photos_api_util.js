7e74840a398534a4c44fd8ff17aa692c0f2fc937
ChristopherALee <christopheradamlee@gmail.com>
2018-01-03 13:43:24 -0500

    Created Photos Api Util

---
+++ frontend/util/photos/photos_api_util.js
@@ -0,0 +1,40 @@
+export const fetchAllPhotos = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/photos'
+  });
+};
+
+export const fetchSinglePhoto = (id) => {
+  return $.ajax({
+    method: 'GET',
+    url: `api/photos/${id}`
+  });
+};
+
+export const createPhoto = (photo) => {
+  return $.ajax({
+    method: 'POST',
+    url: 'api/photos',
+    data: {
+      photo
+    }
+  });
+};
+
+export const updatePhoto = (photo) => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `api/photos/${photo.id}`,
+    data: {
+      photo
+    }
+  });
+};
+
+export const deletePhoto = (id) => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `api/photos/${id}`
+  });
+};
