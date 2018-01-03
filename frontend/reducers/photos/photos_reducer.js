2482d32c451f4662ea48943b6bf266fbb2c8bc0e
ChristopherALee <christopheradamlee@gmail.com>
2018-01-03 14:00:42 -0500

    Created Frontend Photos Reducer

---
+++ frontend/reducers/photos/photos_reducer.js
@@ -0,0 +1,26 @@
+import {
+  RECEIVE_ALL_PHOTOS,
+  RECEIVE_SINGLE_PHOTO,
+  REMOVE_PHOTO } from '../../actions/photos/photo_actions';
+
+const photosReducer = (state = {}, action) => {
+  let newState;
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_ALL_PHOTOS:
+      newState = Object.assign({}, state, action.photos);
+      return newState;
+    case RECEIVE_SINGLE_PHOTO:
+      newState = Object.assign({}, state, {[action.photo.id]: action.photo});
+      return newState;
+    case REMOVE_PHOTO:
+      newState = Object.assign({}, state);
+      delete newState[action.photoId];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default photosReducer;
