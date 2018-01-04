f0cb237c2e31c5a0864694d473da7b005d7c4c4a
ChristopherALee <christopheradamlee@gmail.com>
2018-01-04 18:54:15 -0500

    Fixed user drop down functionality for logout

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -63,12 +63,12 @@ class NavBar extends React.Component {
           />
 
           <div className='user-profile-options'>
-            <i className="fa fa-lg fa-user navbar-right-logout" aria-hidden="true" onClick={this.handleLogOut}>
+            <i className="fa fa-lg fa-user navbar-right-logout" aria-hidden="true">
               <div className='dropdown-break'></div>
               <div className='user-profile-dropdown'>
                 <ul>
                   <li>Profile</li>
-                  <li>Log Out</li>
+                  <li onClick={this.handleLogOut}>Log Out</li>
                 </ul>
               </div>
             </i>
