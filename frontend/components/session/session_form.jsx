8795b66a8990319d68363a3d92cc234dcbca45a6
ChristopherALee <christopheradamlee@gmail.com>
2018-01-05 10:11:26 -0500

    Imported typed.js

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import Typed from 'typed.js';
 
 class SessionForm extends React.Component {
   constructor(props) {
