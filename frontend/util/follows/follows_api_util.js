5d40351480991858c32b17657900e876ac4de7ae
ChristopherALee <christopheradamlee@gmail.com>
2018-01-08 09:43:47 -0500

    Created follows api util

---
+++ frontend/util/follows/follows_api_util.js
@@ -0,0 +1,28 @@
+export const fetchAllFollows = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/follows'
+  });
+};
+
+export const fetchSingleFollow = (id) => {
+  return $.ajax({
+    method: 'GET',
+    url: `api/follows/${id}`
+  });
+};
+
+export const createFollow = (follow) => {
+  return $.ajax({
+    method: 'POST',
+    url: 'api/follows',
+    data: { follow }
+  });
+};
+
+export const removeFollow = (id) => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `api/follows/${id}`
+  });
+};
