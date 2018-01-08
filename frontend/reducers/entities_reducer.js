1eaf993795bff6ac4df9c38b04929b222feac562
ChristopherALee <christopheradamlee@gmail.com>
2018-01-08 10:06:47 -0500

    Added followsReducer onto entitiesReducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './user/user_reducer';
 import photosReducer from './photos/photos_reducer';
+import followsReducer from './follows/follows_reducer';
 
 const entitiesReducer = combineReducers({
   users: userReducer,
-  photos: photosReducer
+  photos: photosReducer,
+  follows: followsReducer,
 });
 
 export default entitiesReducer;
