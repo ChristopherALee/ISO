3580a421e4d1d1d08b7ce68fb4219911ddbcd208
ChristopherALee <christopheradamlee@gmail.com>
2018-01-11 09:13:22 -0500

    Added loadingReducer to rootReducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -4,10 +4,12 @@ import errorsReducer from './errors_reducer';
 import photosReducer from './photos/photos_reducer';
 import userReducer from './user/user_reducer';
 import entitiesReducer from './entities_reducer';
+import loadingReducer from './loading_reducer';
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
   session: sessionReducer,
+  loading: loadingReducer,
   errors: errorsReducer,
 });
 
