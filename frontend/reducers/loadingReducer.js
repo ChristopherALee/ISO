9adebed355e8d0625c7fab474e3b772536fe46f2
ChristopherALee <christopheradamlee@gmail.com>
2018-01-11 09:11:38 -0500

    Added more actions to my reducer to turn off loading spinner when ajax request is finished

--- frontend/reducers/loadingReducer.js
+++ frontend/reducers/loadingReducer.js
@@ -1,12 +1,21 @@
-import { START_LOADING_UPLOAD } from '../actions/photos/photo_actions';
+import {
+  START_LOADING_UPLOAD,
+  RECEIVE_SINGLE_PHOTO,
+  RECEIVE_PHOTO_ERRORS } from '../actions/photos/photo_actions';
 
-const loadingReducer = (state = {}, action) => {
+const loadingReducer = (state = { uploadLoading: false }, action) => {
   let newState;
   Object.freeze(state);
 
   switch (action.type) {
     case START_LOADING_UPLOAD:
       newState = Object.assign({}, state, { uploadLoading: true });
+      return newState;
+    case RECEIVE_SINGLE_PHOTO:
+      newState = Object.assign({}, state, { uploadLoading: false });
+      return newState;
+    case RECEIVE_PHOTO_ERRORS:
+      newState = Object.assign({}, state, { uploadLoading: false });
       return newState;
     default:
       return state;
